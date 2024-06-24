f4543fc77c7b0542e7cd018553fb3ae712ed31d9
Amikola <atte.ikola@gmail.com>
2024-06-24 20:05:37 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+function App() {
+  return (
+  
+    <Router>
+      <p>Hello world</p>
+    </Router>
+   
+  );
+}
+
+export default App;
